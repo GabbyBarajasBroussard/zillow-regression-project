97ef5e00d49306d878292fc1a7df565ea92b969a
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-03-19 13:53:51 -0500

    acquired zillow

--- acquire.py
+++ acquire.py
@@ -12,7 +12,6 @@ import env
 # creating a connection to connect to the Codeup Student Database
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
-In [3]:
 def get_zillow_data():
     '''This function will connect to the Codeup Student Database. It will then cache a local copy to the computer to use for later
         in the form of a CSV file. If you want to reproduce the results, you will need your own env.py file and database credentials.'''
